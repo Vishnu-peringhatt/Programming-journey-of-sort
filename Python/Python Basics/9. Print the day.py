b855ec0b6bfa05d34bf3896407a7b09174c75d22
Vishnu-peringhatt <69888410+Vishnu-peringhatt@users.noreply.github.com>
2023-01-01 15:59:23 +0530

    Create 9. Print the day.py

---
+++ Python/Python Basics/9. Print the day.py
@@ -0,0 +1,26 @@
+#wap to take a number between 1 and 7 from the user .print the day according to the user
+n = int(input (" input a value for the day ( 1-7) : "))
+
+if n == 1 :
+        print (" Sunday" )
+        
+elif n == 2 :
+    print ("Monday")
+    
+elif n == 3 :
+    print ("Tuesday")
+
+elif n == 4:
+    print("Wednesday")
+
+elif n == 5  :
+    print("Thursday")
+elif n == 6 :
+    print("Friday")
+
+elif n == 7 :
+    print("Saturday")
+
+else :
+    print ("invalid number, try again dumbass !")
+    
